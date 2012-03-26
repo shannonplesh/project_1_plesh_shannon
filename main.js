9ac2fdfd6fded70f8d1dab4bea9da35740eb3fd9
shannonplesh <plesh.shannon@gmail.com>
2012-03-26 19:38:41 -0400

    6

---
+++ main.js
@@ -0,0 +1,6 @@
+
+var getHgroup = doctment.getElementsByTagName("hgroup");
+var newHeddingThree = document.createElement("h3");
+var newHeddingText = document.createTextNode("Hedding 4");
+newHedding.appendChild(newHeddingText);
+getHgroup[0].appendChild(newHeddingThree)
